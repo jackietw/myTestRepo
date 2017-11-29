267db8a12b225bc2f4f55961238b8eb874136f7b
Jackie <jackie_tw@msn.com>
2017-11-29 14:51:46 +0800

    * modify build method and add build batch file. * modifiy gitignore

---
+++ buildAll.bat
@@ -0,0 +1,3 @@
+..\ti_components\os_tools\windows\xdctools_3_32_01_22_core\gmake -C .\build\ clean
+..\ti_components\os_tools\windows\xdctools_3_32_01_22_core\gmake -C .\build\ -j depend
+..\ti_components\os_tools\windows\xdctools_3_32_01_22_core\gmake -C .\build\ -j
